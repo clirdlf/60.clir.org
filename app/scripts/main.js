b59c26def10d73d0f543ce39a53ae87040deae1b
Wayne Graham <wgraham@clir.org>
2016-03-02 11:26:24 -0500

    initial site

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
 console.log('\'Allo \'Allo!');
+
+$(window).scroll(function(){
+  handleTopAnimation();
+});
+
+$(window).load(function(){
+    handleTopAnimation();
+});
+
+function handleTopAnimation() {
+  var top = $(window).scrollTop();
+
+  if (top > 10) {
+    $('#site-nav').addClass('navbar-solid');
+  } else {
+    $('#site-nav').removeClass('navbar-solid');
+  }
+}
+
+smoothScroll.init();
