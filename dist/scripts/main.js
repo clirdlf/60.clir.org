32a0e3942e801be7d512728ff098baf111dae385
Wayne Graham <wgraham@clir.org>
2016-03-04 09:46:55 -0500

    Updates to landing page

--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";function handleTopAnimation(){var o=$(window).scrollTop();o>10?$("#site-nav").addClass("navbar-solid"):$("#site-nav").removeClass("navbar-solid")}console.log("'Allo 'Allo!"),$(window).scroll(function(){handleTopAnimation()}),$(window).load(function(){handleTopAnimation()}),smoothScroll.init();
